b91bc480a09c79d34d711742a5c1ae917faa4fa3
tejus <tejus.k.v@gmail.com>
2013-08-25 19:31:54 +0530

    Add device's screen resolution for the new Bootanimation packaging method in vendor_slim

--- slim.mk
+++ slim.mk
@@ -16,6 +16,10 @@
 
 $(call inherit-product, vendor/slim/config/gsm.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
